e766626b7bc0198b440f87ef6df43a2cbc151a2b
Pablo Poo <pablopoo@gmail.com>
2019-01-04 12:13:52 -0300

    Add a spanish installer.

--- Resources/USBInstaller/Windows/build.bat
+++ Resources/USBInstaller/Windows/build.bat
@@ -1,4 +1,5 @@
 "%WIX%bin\candle.exe" resources.wxs setup.wxs
 "%WIX%bin\light.exe" -out OpenRobertaUSBSetupDE.msi -ext WixUIExtension -cultures:de-DE setup.wixobj resources.wixobj -b ./resources
 "%WIX%bin\light.exe" -out OpenRobertaUSBSetupEN.msi -ext WixUIExtension -cultures:en-US setup.wixobj resources.wixobj -b ./resources
+"%WIX%bin\light.exe" -out OpenRobertaUSBSetupES.msi -ext WixUIExtension -cultures:es-ES setup.wixobj resources.wixobj -b ./resources
 @pause
